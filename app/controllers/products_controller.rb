81ba340f1347441d6d61394f0c7ccee4ac7a96fe
Rik Lomas <lomas.rik@gmail.com>
2017-11-26 14:17:59 -0500

    back to original

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,12 +6,6 @@ class ProductsController < ApplicationController
   
   def show
     @product = Product.find(params[:id])
-    
-    @order_item = @current_order.order_items.find_by(product_id: @product.id)
-    
-    if @order_item.nil?
-      @order_item = @current_order.order_items.new(product_id: @product.id, quantity: 1)
-    end
   end
   
 end
